ddb0695c3c60a4660c5fb47754a85de4dbfb9671
chiyaki <66chiyaki@gmail.com>
2017-01-07 18:51:05 +0800

    see all post on root path

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,5 +1,5 @@
 class Account::PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   def index
     @posts = current_user.posts.recent
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
   def index
-    @posts = current_user.posts.recent.paginate(:page => params[:page], :per_page => 5)
+    @posts = Post.all.recent.paginate(:page => params[:page], :per_page => 5)
   end
 end
